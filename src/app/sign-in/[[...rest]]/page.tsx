e3353c1c359b65e65846cc89e132119033247864
ZzyzxIsAlreadyTaken <theoz_@hotmail.com>
2025-02-27 09:28:42 +0100

    Added a sign in route/page

---
+++ src/app/sign-in/[[...rest]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <SignIn />
+    </div>
+  );
+}
